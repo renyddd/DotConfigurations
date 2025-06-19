41553823e3b8400d3f147584f682115557351029
renyddd <renyddd@gmail.com>
2025-06-19 20:42:03 +1000

    update fish

--- config.fish
+++ config.fish
@@ -1,5 +1,2 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    zoxide init fish | source
-    starship init fish | source
-end
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
